23b07ac5f1d11103966058850ab0e9b6e7d8af9c
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-04-25 18:43:49 +0200

    Show the UI of main activity

--- app/src/main/java/com/example/android/myplayer/MainActivity.java
+++ app/src/main/java/com/example/android/myplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.myplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Category> categoryList = new ArrayList<Category>();
+        categoryList.add(new Category(getString(R.string.albums)));
+        categoryList.add(new Category(getString(R.string.audioBooks)));
+        categoryList.add(new Category(getString(R.string.songs)));
+        categoryList.add(new Category(getString(R.string.playLists)));
+
+        CategoryAdapter adapter = new CategoryAdapter(this, categoryList);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
+
+
 }
