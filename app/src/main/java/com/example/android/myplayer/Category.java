634f962382ba7f2981f8eaf0e9c1c28238cf714a
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-04-25 18:42:47 +0200

    Add category class

---
+++ app/src/main/java/com/example/android/myplayer/Category.java
@@ -0,0 +1,16 @@
+package com.example.android.myplayer;
+
+/**
+ * Created by rmhuneineh on 25/04/2017.
+ */
+
+public class Category {
+    private String mCategory;
+
+    public Category(String category){
+        mCategory = category;
+    }
+
+    public String getCategoryName(){ return mCategory; }
+
+}
