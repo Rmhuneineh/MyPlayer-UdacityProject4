fcf773b3101268f09dbbf82c5a998f83dd2ab13d
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-04-25 19:05:12 +0200

    Add audio books activity

---
+++ app/src/main/java/com/example/android/myplayer/AudioBooksActivity.java
@@ -0,0 +1,12 @@
+package com.example.android.myplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AudioBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
