ee2e766d90081fc935e4fcf00ad716f366c55ad1
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-04-28 20:34:32 +0200

    Apply functionality for album activity

--- app/src/main/java/com/example/android/myplayer/AlbumActivity.java
+++ app/src/main/java/com/example/android/myplayer/AlbumActivity.java
@@ -2,6 +2,9 @@ package com.example.android.myplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AlbumActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class AlbumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
+
+        final ArrayList<Album> albums = new ArrayList<Album>();
+        albums.add(new Album(getString(R.string.sampleAlbumName), getString(R.string.sampleArtistName), 1));
+
+        AlbumAdapter adapter = new AlbumAdapter(AlbumActivity.this, albums);
+
+        final ListView listView = (ListView) findViewById(R.id.listView);
+
+        listView.setAdapter(adapter);
     }
 }
